e0ace88ec9fd47853ba190ef7e89d1176741f88d
Marco Pagliarulo <mpaglia1@its.jnj.com>
2022-04-11 17:36:51 +0100

    fix(gatsby-theme-bodiless): cx Image load on safari and gatsby image is generated inside Flow Container

--- packages/cx-image/src/components/Img/tokens/cxImage.ts
+++ packages/cx-image/src/components/Img/tokens/cxImage.ts
@@ -14,6 +14,7 @@
 
 import {
   withNode,
+  withNodeKey,
   withParent,
 } from '@bodiless/core';
 import {
@@ -64,6 +65,9 @@ const Base = asElementToken({
       withGatsbyImageLogger(),
       withoutHydration(),
     ),
+  },
+  Schema: {
+    _: withNodeKey('image'),
   },
   Meta: flowHoc.meta.term('Type')('Image'),
 });
