26d9c1d121bac914b47c4b286e8607f1efea6bd7
Marco Pagliarulo <mpaglia1@its.jnj.com>
2022-11-08 00:41:46 +0000

    fix race condition for data loading

--- packages/bodiless-contentful-editor/src/ContentfulClientDataRetriever.ts
+++ packages/bodiless-contentful-editor/src/ContentfulClientDataRetriever.ts
@@ -1,7 +1,6 @@
 import { BodilessMobxStore } from '@bodiless/core';
 import { createClient } from 'contentful-management';
 import { KnownSDK } from '@contentful/app-sdk';
-import type { EntryProps, CollectionProp } from 'contentful-management';
 
 export const ContentfulClientDataRetriever = async (sdk: KnownSDK, pagePath: string) => {
   const cma = createClient(
@@ -20,52 +19,45 @@ export const ContentfulClientDataRetriever = async (sdk: KnownSDK, pagePath: str
   // Temporary store sdk inside data object in order to pass it to the contentful client store.
   result.set('sdk', sdk);
 
-  await cma.entry.getMany({
+  // Retrieve the page for the current path.
+  const pages = await cma.entry.getMany({
     query: {
       content_type: 'page',
       'fields.path': pagePath,
     }
-  })
-    .then((entries: CollectionProp<EntryProps>) => {
-      if (entries.total > 0) {
-        entries.items.forEach(item => {
-          cma.entry.getMany({
-            query: {
-              content_type: 'path',
-              'fields.path[match]': `pages${item.fields.path['en-US']}`,
-            }
-          })
-            .then((entries: CollectionProp<EntryProps>) => {
-              if (entries.total > 0) {
-                entries.items.forEach(item => {
-                  const pathRegex = new RegExp(`^pages${pagePath.replace('/', '\\/')}`);
-                  if (item.fields.path['en-US'].replace(pathRegex, '').indexOf('/') === -1) {
-                    const key = `Page${BodilessMobxStore.nodeChildDelimiter}${item.fields.path['en-US'].replace('pages/', '')}`;
-                    const data = item.fields.content['en-US'];
-                    result.set(key, data);
-                  }
-                });
-              }
-            });
-        });
+  });
+
+  if (pages.total) {
+    // Retrieve all the nodes for the current path.
+    const nodes = await cma.entry.getMany({
+      query: {
+        content_type: 'path',
+        'fields.path[match]': `pages${pages.items[0].fields.path['en-US']}`,
       }
     });
+    nodes.items.forEach(async (node) => {
+      const pathRegex = new RegExp(`^pages${pagePath.replace('/', '\\/')}`);
+      if (node.fields.path['en-US'].replace(pathRegex, '').indexOf('/') === -1) {
+        const key = `Page${BodilessMobxStore.nodeChildDelimiter}${node.fields.path['en-US'].replace('pages/', '')}`;
+        const data = node.fields.content['en-US'];
+        result.set(key, data);
+      }
+    });
+  }
 
-  await cma.entry.getMany({
+  // Retrieve the page for the current path.
+  const siteEntries = await cma.entry.getMany({
     query: {
       content_type: 'path',
       'fields.path[match]': 'site',
     }
-  })
-    .then((entries: CollectionProp<EntryProps>) => {
-      if (entries.total > 0) {
-        entries.items.forEach(item => {
-          const key = `Site${BodilessMobxStore.nodeChildDelimiter}${item.fields.path['en-US'].replace('site/', '')}`;
-          const data = item.fields.content['en-US'];
-          result.set(key, data);
-        });
-      }
-    });
+  });
+
+  siteEntries.items.forEach(siteItem => {
+    const key = `Site${BodilessMobxStore.nodeChildDelimiter}${siteItem.fields.path['en-US'].replace('site/', '')}`;
+    const data = siteItem.fields.content['en-US'];
+    result.set(key, data);
+  });
 
   return result;
 };
