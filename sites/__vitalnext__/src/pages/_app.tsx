de5d4aeac3dad55ed3681d12efbc14d632d42323
Marco Pagliarulo <marcopagliarulo@gmail.com>
2023-05-04 12:47:41 +0100

    Remove DM-sans helmet link from _app.tsx

--- sites/__vitalnext__/src/pages/_app.tsx
+++ sites/__vitalnext__/src/pages/_app.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
-import { Helmet } from 'react-helmet';
 import '../css/style.css';
 
 function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Helmet>
-        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet" type="text/css" />
-        <Component {...pageProps} />
-      </Helmet>
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default App;
