fda25ee4ab7df48457481b52e4e9796a3fd0af33
hvanyo <hvanyo@kenvue.com>
2023-07-16 10:27:40 -0500

    addscript

--- packages/vital-carousel/src/components/Carousel/index.bl-edit.ts
+++ packages/vital-carousel/src/components/Carousel/index.bl-edit.ts
@@ -1,7 +1,10 @@
 import type { ComponentType } from 'react';
 import { withoutHydration, /* withoutHydrationInline */ } from '@bodiless/hydration';
+
 import type { VitalCarouselProps } from './types';
 import VitalCarouselClean from './VitalCarouselClean';
+
+// import { carouselScript } from './utils/carousel';
 
 /**
  * This clean component is always static.  That means it is never hydrated
@@ -10,7 +13,20 @@ import VitalCarouselClean from './VitalCarouselClean';
  * If anything inserted into a component slot (or any children) requires
  * interactivity, Use VitalCarouselClean instead.
  */
-const VitalCarouselStatic: ComponentType<VitalCarouselProps> = withoutHydration()(
+
+// Referfence Type to pass in:
+// export type WithoutHydrationOptions = {
+//   onUpdate?: (props: Record<string, any>, element: HTMLElement | null) => void
+//   WrapperStyle?: CSSProperties
+//   WrapperElement: 'div'|'span',
+// };
+
+const options = {};
+// {
+//  onUpdate: (carouselScript)
+// };
+
+const VitalCarouselStatic: ComponentType<VitalCarouselProps> = withoutHydration(options)(
   VitalCarouselClean
 );
 // @TODO Use withoutHydrationInline if your component renders inline
